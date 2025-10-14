e6f0c6ec6796b30527ac40e2f471e0f40861a7d7
Hdz-JC <a22150084@itmexicali.edu.mx>
2025-10-13 21:20:45 -0700

    Codigo actualizado al dia 13/10/2025

---
+++ lib/screen/chat/chat_screen.dart
@@ -0,0 +1,22 @@
+import 'package:chat_boggy/views/chat_view.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget{
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BoggyChat'),
+        centerTitle: false,
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(
+            'https://i.pinimg.com/736x/10/52/84/105284eb3a3446c5f3374be1150362e9.jpg'
+          ),
+        ),
+      ),
+      body: ChatView(),
+    );
+  }
+}
